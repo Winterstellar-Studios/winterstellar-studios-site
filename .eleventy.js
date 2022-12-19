10363d5e3e22b614f8dcc870b48fc2341bb3f39c
Aidan Jones <aidan@aidanjon.es>
2022-12-18 19:45:27 -0800

    Eleventy config code to redirect to 404 page in local environment

--- .eleventy.js
+++ .eleventy.js
@@ -2,8 +2,30 @@ const yaml = require("js-yaml");
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const htmlmin = require("html-minifier");
+const fs = require("fs");
+const NOT_FOUND_PATH = "_site/404.html";
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          if (!fs.existsSync(NOT_FOUND_PATH)) {
+            throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
+          }
+
+          const content_404 = fs.readFileSync(NOT_FOUND_PATH);
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
+  });
+
   // Disable automatic use of your .gitignore
   eleventyConfig.setUseGitIgnore(false);
 
